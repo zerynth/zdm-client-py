d64c96a1ce7dd2bb1200bc2010ab8654fc65608f
dneri <d.neri@zerynth.com>
2020-03-27 11:33:05 +0100

    docs rst

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requirements = [
 
 setuptools.setup(
     name="zdm-client-py",
-    version="0.0.1",
+    version="0.0.2",
     author="Zerynth Team",
     author_email="d.neri@zerynth.com",
     description="ZDM Client Python Library",
