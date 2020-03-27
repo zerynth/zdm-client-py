c59fe1fcc1d962e666ee71621f856c5bdcd7f4f5
dneri <d.neri@zerynth.com>
2020-03-27 11:47:26 +0100

    added python 3 support

---
+++ HISTORY.rst
@@ -0,0 +1,11 @@
+=======
+History
+=======
+
+0.0.3 (2020-03-27)
+----------------------------
+Release with support Python >3
+
+* Create ZDmClient
+* Publish messages to ZDM
+* Received Jobs from ZDM
--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
 requirements = [
     "paho-mqtt",
     "click"
@@ -10,11 +13,11 @@ requirements = [
 
 setuptools.setup(
     name="zdm-client-py",
-    version="0.0.2",
+    version="0.0.3",
     author="Zerynth Team",
     author_email="d.neri@zerynth.com",
     description="ZDM Client Python Library",
-    long_description=long_description,
+    long_description=long_description + '\n\n' + history,
     long_description_content_type="text/markdown",
     url="https://github.com/zerynth/zdm-client-py.git",
     packages=setuptools.find_packages(),
