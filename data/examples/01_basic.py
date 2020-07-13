c4e931ed5dbcce125954aa5436ecb2ebe97976b4
e.neri <e.neri@zerynth.com>
2020-07-13 16:09:54 +0200

    fix example 01

--- data/examples/01_basic.py
+++ data/examples/01_basic.py
@@ -2,50 +2,33 @@
 basic.py
 
 Show the basic example of a ZdmClient that sends a stream of messages to the ZDM.
-Each message is published into a random tag with a random temperature value.
+Each message is published into a random tag with a random value.
 
 """
+import zdm
 import random
 import time
-import zdm
 
-device_id = 'your device id'
-password = 'device password'    
-
-def pub_random():
-    # this function is called periodically to publish to ZDM random int value labeled with tags values
-    print('------ publish random ------')
-    tags = ['tag1', 'tag2', 'tag3']
-
-    for t in tags:
-        value = random.randint(0, 20)
-        payload = {
-            'value': value
-        }
-        # publish payload to ZDM
-        device.publish_data(t, payload)
-        print('published on tag:', t, ':', payload)
-
-    print('pub_random done')
+device_id = 'Your-device-id'
+password = 'Device-Password'
 
 
-def pub_temp_pressure():
-    # this function publish another payload with two random int values
-    print('---- publish temp_pressure ----')
-    tag = 'tag4'
-    temp = random.randint(19, 23)
-    pressure = random.randint(50, 60)
-    payload = {'temp': temp, 'pressure': pressure}
-    device.publish_data(tag, payload)
-    print('published on tag: ', tag, ':', payload)
+def pub_temp_hum():
+    # this function publish into the tag weather two values: the temperature and the humidity
+    tag = 'weather'
+    temp = random.randint(19, 38)
+    hum = random.randint(50, 70)
+    payload = {'temp': temp, 'hum': hum}
+    device.publish(payload, tag)
+    print('Published: ', payload)
 
 
-device = zdm.ZDMClient(device_id=device_id,  endpoint="rmq.localhost")
+# connection to the ZDM
+device = zdm.ZDMClient(device_id=device_id)
 device.set_password(password)
 device.connect()
 
-
+# infinite loop
 while True:
-    time.sleep(2)
-    pub_random()
-    pub_temp_pressure()
+    pub_temp_hum()
+    time.sleep(5)
